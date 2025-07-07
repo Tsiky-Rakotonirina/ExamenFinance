3c13f1d98572145b8dbf13c165bc13e07309a517
Tsiky-Rakotonirina <tsikyrakotonirina20@gmail.com>
2025-07-07 17:42:52 +0300

    prepare next feature

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="template.php?page=statistique-interet">STATISTIQUES</a>
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title></title>
+    <link rel="icon" type="image/x-icon" href="<?=$url ?>"/>
+    <link rel="stylesheet" href="css/template.css">
+    <script>const urlBase = 'http://localhost:8888/ExamenFinance/ws'; </script>
+    <script src="js/template.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div id="loader">
+        <div></div>
+    </div>
+    <header>
+        <nav class="menu">
+            
+        </nav>
+    </header>
+    <main>
+        <?php include($_GET["page"].".php") ?>    
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
