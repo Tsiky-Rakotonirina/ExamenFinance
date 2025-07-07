0ae941fc6c144d37566e24e7085a580bd565d717
Tsiky-Rakotonirina <tsikyrakotonirina20@gmail.com>
2025-07-07 17:26:39 +0300

    nouveau architecture

--- public/assets/fpdf/connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host='localhost';
-    $dbname='pdf';
-    $user='root';
-    $pass='';
-    $bdd = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-?>
