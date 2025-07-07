5efd320a821b5ecdcf52cb86ce0b5df080adc230
Etsoa <fetraniaina26@gmail.com>
2025-07-07 17:44:49 +0300

    suppression de pretClient.js

--- app/views/template.php
+++ app/views/template.php
@@ -14,9 +14,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" >
     <script>const urlBase = '<?= $url ?>'; </script>
 
-    <script src="<?=$url ?>/public/assets/js/gestionPret/pretClient.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=$url ?>/public/assets/js/index.js"></script>
 </head>
 <body>
     <header>
